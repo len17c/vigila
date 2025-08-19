5f3fdb63ce8701e1813eadab0f03250453d5962d
lenxxx17cm <lenxxx17cm@gmail.com>
2025-08-19 20:20:41 +0000

    una vez mas

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import os
-
-from flask import Flask
+import time
+from flask import *
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-  """Example Hello World route."""
-  name = os.environ.get("NAME", "World")
-  return f"Hello {name}!"
+@app.route("/", methods = ['GET', 'POST'])
+def inicio():
+  datos = []
+  if request.method == "POST":
+    vig = request.form['id']
+    sec = request.form['seccion']
+    est = request.form['estado']
+    ano = request.form['anotaciones']
+    guardar(vig, sec, est, ano)
+    
+  return render_template("index.html")
+
+@app.route("/resumen")
+def resumen():
+  datos = []
+  archivo = open('log.csv', 'r')
+  for linea in archivo:
+    dato = linea.split(';')
+    datos.append({"fecha": dato[0], "vigilante": dato[1], "seccion": dato[2], "estado": dato[3], "anotacion": dato[4]})
+  archivo.close()
+  return render_template("resumen.html", datos = datos)
+
+def guardar(a, b, c, d):
+  fecha = time.strftime("%d/%m/%Y %H:%M:%S")
+  archivo = open('log.csv', 'a')
+  archivo.write(f'{fecha};{a};{b};{c};{d}\n')
+  archivo.close()
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 3000)))
